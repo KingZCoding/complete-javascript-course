95f31fe4d152a65f625015b9acb9093fb63b9523
KingZCoding <traenknercody@gmail.com>
2024-09-13 10:17:25 -0400

    forgot to upload finish pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,97 @@
 'use strict';
+
+// selecting elements
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
+
+const playerScore0 = document.querySelector('#score--0');
+const playerScore1 = document.querySelector('#score--1');
+// selecting current player
+const current0 = document.querySelector('#current--0');
+const current1 = document.querySelector('#current--1');
+// selecting dice
+const dice = document.querySelector('.dice');
+// selecting btns
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  currentScore = 0;
+  activePlayer = 0;
+  scores = [0, 0];
+  playing = true;
+
+  current0.textContent = 0;
+  current1.textContent = 0;
+  playerScore0.textContent = 0;
+  playerScore1.textContent = 0;
+
+  player0.classList.remove('player--winner');
+  player1.classList.remove('player--winner');
+  player0.classList.add('player--active');
+  player1.classList.remove('player--active');
+  dice.classList.add('hidden');
+};
+init();
+
+const switchPlayer = function () {
+  //   switch to next player
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0.classList.toggle('player--active');
+  player1.classList.toggle('player--active');
+};
+
+const setScore = function () {};
+
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // 1. generating a random dice roll
+    const rollDice = Math.floor(Math.random() * 6) + 1;
+    // 2. display dice
+    dice.classList.remove('hidden');
+    dice.src = `dice-${rollDice}.png`;
+
+    // 3. check for rolled 1: if true swich to next player
+    if (rollDice !== 1) {
+      // add dice to curent score
+      currentScore += rollDice;
+      //   select score on which players turn it is
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+      // current0.textContent = currentScore; //Change later to display to current player
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. add current score of active player
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // 2. check if score is 100 and end game
+    if (scores[activePlayer] >= 100) {
+      // finish the game
+      playing = false;
+      dice.classList.add('hidden');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // 3. switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', init);
--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,117 @@
 'use strict';
+
+/*
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // creating new variable with same name as outer scopes variable
+      const firstName = 'Steven';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+      //   reassinging outer scopes variable
+      output = 'New Output';
+    }
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Aiden';
+
+calcAge(1991);
+
+// hoisting with variables
+var me = 'Aiden';
+let job = 'teacher';
+const year = 1991;
+
+console.log(me);
+console.log(job);
+console.log(year);
+
+// hoisting with functions
+
+// console.log(addArrow);
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// example
+
+if (!numProducts) deleteShoppingCart();
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+
+// console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  //   console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(2023);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
+*/
+const jonas = {
+  year: 1991,
+  firstName: 'Jonas',
+
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+  greet: () => console.log(`Hey ${this.firstName}`),
+};
+jonas.greet();
